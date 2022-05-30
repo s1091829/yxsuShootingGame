fa6b89e0329b6a868c5382dad8cdfb9f8c484f94
Su <s1091829@gm.pu.edu.tw>
2022-05-31 01:09:41 +0800

    v.2(圖形按鈕及捲動背景)

--- app/src/main/java/tw/edu/pu/csim/s1091829/yxsushootinggame/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1091829/yxsushootinggame/MainActivity.kt
@@ -1,16 +1,50 @@
 package tw.edu.pu.csim.s1091829.yxsushootinggame
 
 import android.content.pm.ActivityInfo
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var img : ImageView
+    lateinit var game : Game
+    var flag:Boolean = false
+    lateinit var job : Job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //設定螢幕水平顯示
         //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+        img = findViewById(R.id.img)
+        game = findViewById(R.id.game)
+
+        img.setOnClickListener( {
+            if (flag){
+                flag = false
+                img.setImageResource(R.drawable.start)
+                job.cancel()
+            }
+            else{
+                flag = true
+                img.setImageResource(R.drawable.stop)
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    while(flag) {
+                        delay(10)
+                        var canvas: Canvas = game.surfaceHolder.lockCanvas()
+                        game.drawSomething(canvas)
+                        game.surfaceHolder.unlockCanvasAndPost(canvas)
+                    }
+                }
+            }
+
+        })
 
     }
 }
